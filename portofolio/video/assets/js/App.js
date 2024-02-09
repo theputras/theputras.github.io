30303d1168087e01c92ab5b74f6e673f6fd26ddb
theputras <theputras.media@gmail.com>
2024-02-09 19:26:45 +0700

    Update

--- portofolio/video/assets/js/App.js
+++ portofolio/video/assets/js/App.js
@@ -1,6 +1,9 @@
 // Change "{}" to your options:
 // https://github.com/sampotts/plyr/#options
-const player = new Plyr('#player', {});
+const player = new Plyr('#player', {
+ 
+});
 
 // Expose player so it can be used from the console
 window.player = player;
+
