a8f51d69c2266ba5211798feec44ad408be654af
theputras <theputras.media@gmail.com>
2024-04-02 11:43:48 +0700

    Update

--- assets/js/google-sheet.js
+++ assets/js/google-sheet.js
@@ -1,8 +1,8 @@
-const scriptURL = 'https://script.google.com/macros/s/AKfycbzlNGLPq5LdDgk2AJIfVLZU1ox7Uja2ahXYU3-ofFHoKc7wAJAzpWgrl0slRJRNBy3P/exec'
+const scriptURL = 'https://script.google.com/macros/s/AKfycbx-ZzfbneGsZ5Bs95-xHMuSBG5vOBtiC2sRFbvHrDk5AUX0NebtHTbGWMULv-x6FAqs/exec'
 const form = document.forms['submit-to-google-sheet']
-
 const sucess = document.getElementById('success')
 const progressBar = document.getElementById('progressBar');
+const url = './loading'
 
 form.addEventListener('submit', e => {
   e.preventDefault()
@@ -14,6 +14,9 @@ setTimeout(() => {
   }, 3000); // Setelah 3 detik, sembunyikan progress bar
 }, 1000); // Setelah 1 detik, perbarui progress bar ke 100%
   fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-    
-    .catch(error => console.error('Error!', error.message))
+
+  .catch(error => console.error('Error!', error.message))
 })
+
+
+
