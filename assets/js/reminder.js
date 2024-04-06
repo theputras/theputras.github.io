8b70fd780f7f4dcb638be681aa23fae4e4dfba39
theputras <149865892+theputras@users.noreply.github.com>
2024-04-06 13:34:40 +0700

    Update reminder.js

--- assets/js/reminder.js
+++ assets/js/reminder.js
@@ -1,5 +1,5 @@
 // Set the date we're counting down to
-var countDownDate = new Date("Apr 10, 202 00:00:00");
+var countDownDate = new Date("Apr 10, 2024 00:00:00");
 
 // Update the count down every 1 second
 var x = setInterval(function() {
